b18ca2a18f3db9032723325a653075b673ea3d58
luhaoqi <1171821456@qq.com>
2022-11-10 00:01:03 +0800

    finish part 2, use std::mutex and std::condition_variable

--- lock_server.cc
+++ lock_server.cc
@@ -1,37 +1,53 @@
 // the lock server implementation
 
 #include "lock_server.h"
-#include <sstream>
+
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
-lock_server::lock_server():
-  nacquire (0)
-{
-}
+#include <sstream>
 
-lock_protocol::status
-lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
-{
+lock_server::lock_server() : nacquire(0) {}
+
+lock_protocol::status lock_server::stat(int clt, lock_protocol::lockid_t lid,
+                                        int &r) {
   lock_protocol::status ret = lock_protocol::OK;
   printf("stat request from clt %d\n", clt);
   r = nacquire;
   return ret;
 }
 
-lock_protocol::status
-lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
-{
+lock_protocol::status lock_server::acquire(int clt, lock_protocol::lockid_t lid,
+                                           int &r) {
   lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2B part2 code goes here
+  // Your lab2B part2 code goes here
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    // 该锁还没有使用过 返回该锁 true表示上锁
+    if (lck_manager.find(lid) == lck_manager.end()) {
+      lck_manager[lid] = true;
+    } else {
+      // while循环 防止虚假唤醒
+      while (lck_manager[lid]) cv.wait(lck);
+      lck_manager[lid] = true;
+    }
+  }
   return ret;
 }
 
-lock_protocol::status
-lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
-{
+lock_protocol::status lock_server::release(int clt, lock_protocol::lockid_t lid,
+                                           int &r) {
   lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2B part2 code goes here
+  // Your lab2B part2 code goes here
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    if (lck_manager.find(lid) == lck_manager.end()) {
+      ret = lock_protocol::NOENT;
+    } else {
+      lck_manager[lid] = false;
+      cv.notify_one();
+    }
+  }
   return ret;
 }
--- lock_server.h
+++ lock_server.h
@@ -4,22 +4,31 @@
 #ifndef lock_server_h
 #define lock_server_h
 
+#include <condition_variable>
 #include <string>
-#include "lock_protocol.h"
+#include <thread>
+
 #include "lock_client.h"
+#include "lock_protocol.h"
 #include "rpc.h"
 
 class lock_server {
-
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, bool> lck_manager;
+  std::condition_variable cv;
+  // 对map的锁，可以实现不同inum的并行，可以扩展成一个map多个锁，增加并行程度
+  std::mutex mtx;
+
+  // pthread_mutex_t mutex;
+  // static pthread_cond_t cond;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server(){};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
   lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
   lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
-#endif 
+#endif
